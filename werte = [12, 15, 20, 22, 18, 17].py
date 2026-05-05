d1d6ef5126bb0294bc680d5c23222aba4814a3be
Isi1305 <isabell.sophieandrea@gmail.com>
2026-05-05 14:00:52 +0200

    Verschiedener Code eingefügt mit ein paar Kommentaren.

---
+++ werte = [12, 15, 20, 22, 18, 17].py
@@ -0,0 +1,40 @@
+werte = [12, 15, 20, 22, 18, 17]
+print(max(werte))
+print(min(werte))
+
+average = sum(werte) / len(werte)
+print(average)
+
+for wert in werte: #Die Schleife iteriert über jedes Element in der Liste "werte"
+    if wert > average: #Vergleicht den aktuellen Wert mit dem Durschnitt
+        print(f"{wert} ist über dem Durschnitt.")
+    elif wert < average:
+        print(f"{wert} ist unter dem Durschnitt.")
+    elif wert == average:
+        print(f"{wert} ist gleich dem Durschnitt.")
+
+if werte[-1] > werte[0]:
+    print("Trend steigend")
+elif werte[-1] < werte[0]:
+    print("Trend fallend")
+else:
+    print("Trend stabil")
+
+for i in range(len(werte) - 1): #Die Schleife läuft von 0 bis zur vorletzten Position.
+    if werte[i +1] > werte[i]: #Vergleich der Werte, um den Trend zu bestimmen #Für mich: i + 1 = „Geh eine Position weiter nach rechts in der Liste“
+        print(f"Von {werte[i] } zu {werte[i + 1]}: steigend")
+    elif werte [i + 1] < werte[i]:
+        print(f"Von {werte[i]} zu {werte[i + 1]}: fallend")
+    elif werte[i + 1] == werte[i]:
+        print(f"Von {werte[i]} zu {werte[i + 1]}: stabil")
+
+score = 0 #Variable, die den Trend bewertet. Sie wird um 1 erhöht, wenn der Trend strigt und verringert, wenn er fällt.
+for i in range(len(werte) - 1):
+    if werte[i +1] > werte[i]:
+        score += 1
+    elif werte [i + 1] < werte[i]:
+        score -= 1
+    elif werte[i + 1] == werte[i]: #Unnötig, da der Score sich nit verändert, aber gut als Übung.
+        score += 0
+
+print(f"Trend Score: {score}")
